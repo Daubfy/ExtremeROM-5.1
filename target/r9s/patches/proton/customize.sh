84f5d9cdd8f17235d1e5af11ead1595d62c70fdf
Daubfy <empresasegsocialsas@gmail.com>
2025-11-20 12:17:44 -0500

    Update r9s patches, TEE configs, props
    Co-authored-by: majaahh <majaahh@users.noreply.github.com>
    Co-authored-by: salvogiangri <salvogiangri@users.noreply.github.com>

---
+++ target/r9s/patches/proton/customize.sh
@@ -0,0 +1,2 @@
+cp -fa "$SRC_DIR/target/r9s/patches/proton/img/boot.img" "$WORK_DIR/kernel/boot.img"
+cp -fa "$SRC_DIR/target/r9s/patches/proton/img/vendor_boot.img" "$WORK_DIR/kernel/vendor_boot.img"
