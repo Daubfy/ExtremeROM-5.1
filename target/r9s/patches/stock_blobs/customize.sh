f1a78d430b1784f70597bfc1db8ded681f0cd01e
Daubfy <empresasegsocialsas@gmail.com>
2025-12-01 11:25:51 -0500

    replace libhwui libs

--- target/r9s/patches/stock_blobs/customize.sh
+++ target/r9s/patches/stock_blobs/customize.sh
@@ -23,3 +23,10 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/GameDriver-EX2100/G
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/DevGPUDriver-EX2100/DevGPUDriver-EX2100.apk" 0 0 644 "u:object_r:system_file:s0"
 LOG_STEP_OUT
 
+LOG_STEP_IN "- Replacing libhwui"
+DELETE_FROM_WORK_DIR "system" "system/lib/libhwui.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libhwui.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libhwui.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhwui.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
+
