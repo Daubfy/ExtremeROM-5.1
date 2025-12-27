0ace2bd3bd29fae061910c4dd0375b1cca88ca89
Daubfy <empresasegsocialsas@gmail.com>
2025-12-27 09:14:10 -0500

    remove vendor_dlkm before making rom zip

--- target/a24/patches/kernel/customize.sh
+++ target/a24/patches/kernel/customize.sh
@@ -1,2 +1,3 @@
 cp -fa "$SRC_DIR/target/a24/patches/kernel/img/boot.img" "$WORK_DIR/kernel/boot.img"
 cp -fa "$SRC_DIR/target/a24/patches/kernel/img/vendor_boot.img" "$WORK_DIR/kernel/vendor_boot.img"
+rm -rf "$WORK_DIR/vendor_dlkm"
