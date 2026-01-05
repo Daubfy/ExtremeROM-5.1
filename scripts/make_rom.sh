4413b98a632ff95a6e1ce80c1bdcd27633f3d9f5
Daubfy <empresasegsocialsas@gmail.com>
2026-01-05 12:15:52 -0500

    remove debugging stuff and apktool (TMP)

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -87,7 +87,6 @@ if $BUILD_ROM; then
         bash "$SRC_DIR/scripts/download_fw.sh"
         bash "$SRC_DIR/scripts/extract_fw.sh"
     fi
-    read -p "Breakpoint after extracting the firmware Detected! Press Enter to continue..."
 
     echo -e "- Creating work dir..."
     bash "$SRC_DIR/scripts/internal/create_work_dir.sh"
@@ -100,14 +99,13 @@ if $BUILD_ROM; then
     echo -e "\n- Applying ROM mods..."
     bash "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/unica/mods"
 
-    echo -e "\n- Recompiling APKs/JARs..."
-    while read -r i; do
-        bash "$SRC_DIR/scripts/apktool.sh" b "$i"
-    done <<< "$(find "$OUT_DIR/apktool" -type d \( -name "*.apk" -o -name "*.jar" \) -printf "%p\n" | sed "s.$OUT_DIR/apktool..")"
+    #echo -e "\n- Recompiling APKs/JARs..."
+    #while read -r i; do
+    #    bash "$SRC_DIR/scripts/apktool.sh" b "$i"
+   # done <<< "$(find "$OUT_DIR/apktool" -type d \( -name "*.apk" -o -name "*.jar" \) -printf "%p\n" | sed "s.$OUT_DIR/apktool..")"
 
     echo ""
     echo -n "$WORK_DIR_HASH" > "$WORK_DIR/.completed"
-    read -p "Breakpoint after creating the workdir Detected! Press Enter to continue..."
 else
     echo -e "- Nothing to do in work dir.\n"
 fi
