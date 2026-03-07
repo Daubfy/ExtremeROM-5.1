4105e60808dfc173f399a7c734f8ef8959942bb1
Daubfy <149339273+Daubfy@users.noreply.github.com>
2026-03-07 15:00:22 -0500

    Update customize.sh
    fix for https://github.com/Daubfy/UN1CA/actions/runs/22805464490/job/66153765321#step:12:533

--- target/a24/patches/simfix/customize.sh
+++ target/a24/patches/simfix/customize.sh
@@ -1,4 +1,4 @@
 {
     echo "(type sim, device, chr_file, (write getattr));"
     echo "(type sim, init, sock_file, (write getattr));"
-} >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil
+} >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
